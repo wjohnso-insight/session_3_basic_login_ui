dadd912ebb38d75c949dd42bcdcdc3fd9a3e70d9
Will <will.johnston@insight.com>
2021-02-17 12:35:47 -0500

    Added FormsModule
    1. Imported FormsModule from @angular/core
    2. Added FormsModule to imports array
    
    This resolved error TS2322: Type 'Event' is not assignable to type 'string'

--- basic-login-ui/src/app/app.module.ts
+++ basic-login-ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { LoginCardComponent } from './login-card/login-card.component';
@@ -18,7 +19,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
