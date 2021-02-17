4cfbb2c465eadd6470ea70bb42b50061baf1e203
Will <will.johnston@insight.com>
2021-02-17 10:44:14 -0500

    Added fontawesome to class definition
    1. Imported faUser icon from @fortawesome
    2. Added an faUser property equal to imported icon

--- basic-login-ui/src/app/circle-icon/circle-icon.component.ts
+++ basic-login-ui/src/app/circle-icon/circle-icon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-circle-icon',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class CircleIconComponent implements OnInit {
 
   constructor() { }
+
+  faUser = faUser;
 
   ngOnInit(): void {
   }
