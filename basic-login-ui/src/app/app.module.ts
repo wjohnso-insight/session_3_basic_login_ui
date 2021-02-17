e4ec14059520781c5c68d072c527f5be7db985f3
Will <will.johnston@insight.com>
2021-02-16 20:33:22 -0500

    Created CircleIcon Component & LoginForm Component
    1. Created CircleIcon Component and added to LoginCard template
    2. Created LoginForm Component and added to LoginCard template

--- basic-login-ui/src/app/app.module.ts
+++ basic-login-ui/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginCardComponent } from './login-card/login-card.component';
+import { CircleIconComponent } from './circle-icon/circle-icon.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginCardComponent
+    LoginCardComponent,
+    CircleIconComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
